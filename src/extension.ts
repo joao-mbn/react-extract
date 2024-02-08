043903d3c75d37cd8f353058f65e2215faeec1db
João Nascimento <joaombn97@gmail.com>
2024-02-08 19:26:04 -0300

    Removed template comments.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 class ExtractOnRefactorProvider implements vscode.CodeActionProvider {
@@ -17,11 +15,7 @@ class ExtractOnRefactorProvider implements vscode.CodeActionProvider {
   }
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "extract" is now active!');
 
   context.subscriptions.push(
@@ -30,9 +24,6 @@ export function activate(context: vscode.ExtensionContext) {
     })
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("extract.extractComponent", () => {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
@@ -51,5 +42,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
