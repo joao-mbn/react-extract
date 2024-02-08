1ff27901ba94cd51bc465f78c2200f7d817a7697
João Nascimento <joaombn97@gmail.com>
2024-02-08 19:25:11 -0300

    Created CodeAction to show extractComponent command on Refactor menu.

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,54 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+class ExtractOnRefactorProvider implements vscode.CodeActionProvider {
+  provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range | vscode.Selection,
+    context: vscode.CodeActionContext,
+    token: vscode.CancellationToken
+  ) {
+    const refactor = new vscode.CodeAction("Extract Component", vscode.CodeActionKind.Refactor);
+
+    refactor.command = { command: "extract.extractComponent", title: "Extract Component", arguments: [document, range] };
+
+    return [refactor];
+  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "extract" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extract" is now active!');
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider([{ pattern: "{**/*.js,**/*.ts,**/*.jsx,**/*.tsx}" }], new ExtractOnRefactorProvider(), {
+      providedCodeActionKinds: [vscode.CodeActionKind.Refactor],
+    })
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extract.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Extract!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("extract.extractComponent", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-	context.subscriptions.push(disposable);
+    const selection = editor.selection;
+    const text = editor.document.getText(selection);
+    if (!text) {
+      return;
+    }
+
+    console.log(`Selected text: ${text}`);
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
