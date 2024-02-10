7e641060ce2c0a5b5c0437751fb78db4cb78b7cc
João Nascimento <joaombn97@gmail.com>
2024-02-09 22:24:36 -0300

    Added logic to get the props out of extracted components, add the props to the new component's arguments and to its interface.

--- src/extractComponent.ts
+++ src/extractComponent.ts
@@ -5,24 +5,39 @@ import { isFileTypescript } from "./checks";
 export function extractComponent(document: vscode.TextDocument, range: vscode.Range | vscode.Selection) {
   const selectedText = document.getText(range).trim();
 
-  // Get file extension
   const isTypescript = isFileTypescript(document);
+
+  // assuming the selection is JSX, extract props and props values, like <Component prop1="value1" prop2={value2} />
+  const propsRegex = /(\w+)\s*=\s*({[^}]+}|".*"|'.*')/g;
+  const propsMatches = selectedText.match(propsRegex);
+
+  const entries =
+    propsMatches?.map((match) => {
+      const [prop, value] = match.split("=");
+      return { prop: prop.trim(), value: value.trim() };
+    }) ?? [];
 
   let content = "";
 
-  // Add interface Component1Props if file is a TypeScript file
-  const componentName = "Component1";
-  const interfaceName = `${componentName}Props`;
-  if (isTypescript) {
-    content += dedent`
-      interface ${interfaceName} {
+  // Create a component with the name Component{5 random numbers/letters}
+  const componentName = `Component_${Math.random().toString(36).substring(2, 7).toUpperCase()}`;
 
+  // Add interface Component1Props if file is a TypeScript file
+  const interfaceName = `${componentName}Props`;
+
+  if (isTypescript) {
+    content += dedent`\n
+      interface ${interfaceName} {
+        ${entries
+          .map((e) => `${e.prop}: unknown`)
+          .join(";\n")
+          .concat(";")}
       }\n\n
     `;
   }
 
   content += dedent`
-    function ${componentName} ({ ...props }${isTypescript ? `: ${interfaceName}` : ""}) {
+    function ${componentName} ({ ${entries.map((e) => e.prop).join(", ")} }${isTypescript ? `: ${interfaceName}` : ""}) {
       return (
         ${selectedText}
       );
