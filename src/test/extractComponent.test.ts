82c9c674d4b8b0e2f7927eb73dd157a6ea266b6b
João Nascimento <joaombn97@gmail.com>
2024-02-11 17:40:50 -0300

    Standardizing test assertions with expected, result.

--- src/test/extractComponent.test.ts
+++ src/test/extractComponent.test.ts
@@ -11,8 +11,6 @@ function strictEqualStrippingLineBreaks(expected: string, result: string) {
   return assert.strictEqual(parser(expected), parser(result));
 }
 
-// TODO: invert the order of arguments where results comes first
-
 suite("extractProps", () => {
   test("should handle props with different value types", () => {
     const selectedText = `
@@ -21,13 +19,16 @@ suite("extractProps", () => {
       </Component>
     `;
     const result = extractProps(selectedText);
-    unorderedDeepStrictEqual(result, [
-      { pair: "prop1={true}", prop: "prop1", value: "{true}", propAlias: "prop1" },
-      { pair: 'prop2={"string"}', prop: "prop2", value: '{"string"}', propAlias: "prop2" },
-      { pair: "prop3={'string'}", prop: "prop3", value: "{'string'}", propAlias: "prop3" },
-      { pair: "prop4='string'", prop: "prop4", value: "'string'", propAlias: "prop4" },
-      { pair: 'prop5="string"', prop: "prop5", value: '"string"', propAlias: "prop5" },
-    ]);
+    unorderedDeepStrictEqual(
+      [
+        { pair: "prop1={true}", prop: "prop1", value: "{true}", propAlias: "prop1" },
+        { pair: 'prop2={"string"}', prop: "prop2", value: '{"string"}', propAlias: "prop2" },
+        { pair: "prop3={'string'}", prop: "prop3", value: "{'string'}", propAlias: "prop3" },
+        { pair: "prop4='string'", prop: "prop4", value: "'string'", propAlias: "prop4" },
+        { pair: 'prop5="string"', prop: "prop5", value: '"string"', propAlias: "prop5" },
+      ],
+      result
+    );
   });
 
   test("should handle props with complex values", () => {
@@ -37,11 +38,14 @@ suite("extractProps", () => {
       </Component>
     `;
     const result = extractProps(selectedText);
-    unorderedDeepStrictEqual(result, [
-      { pair: "prop1={{ key: 'value' }}", prop: "prop1", value: "{{ key: 'value' }}", propAlias: "prop1" },
-      { pair: "prop2={[1, 2, 3]}", prop: "prop2", value: "{[1, 2, 3]}", propAlias: "prop2" },
-      { pair: "prop3={() => { doStuff(); }}", prop: "prop3", value: "{() => { doStuff(); }}", propAlias: "prop3" },
-    ]);
+    unorderedDeepStrictEqual(
+      [
+        { pair: "prop1={{ key: 'value' }}", prop: "prop1", value: "{{ key: 'value' }}", propAlias: "prop1" },
+        { pair: "prop2={[1, 2, 3]}", prop: "prop2", value: "{[1, 2, 3]}", propAlias: "prop2" },
+        { pair: "prop3={() => { doStuff(); }}", prop: "prop3", value: "{() => { doStuff(); }}", propAlias: "prop3" },
+      ],
+      result
+    );
   });
 
   test("should handle repeated props", () => {
@@ -50,11 +54,14 @@ suite("extractProps", () => {
         <NestedComponent className={value2} prop={value}/>
       </Component>`;
     const result = extractProps(selectedText);
-    unorderedDeepStrictEqual(result, [
-      { pair: "className={value1}", prop: "className", value: "{value1}", propAlias: "className" },
-      { pair: "className={value2}", prop: "className", value: "{value2}", propAlias: "className2" },
-      { pair: "prop={value}", prop: "prop", value: "{value}", propAlias: "prop" },
-    ]);
+    unorderedDeepStrictEqual(
+      [
+        { pair: "className={value1}", prop: "className", value: "{value1}", propAlias: "className" },
+        { pair: "className={value2}", prop: "className", value: "{value2}", propAlias: "className2" },
+        { pair: "prop={value}", prop: "prop", value: "{value}", propAlias: "prop" },
+      ],
+      result
+    );
   });
 
   test("should handle props with deeply nested values and repeated props", () => {
@@ -74,29 +81,32 @@ suite("extractProps", () => {
       </Component>
     `;
     const result = extractProps(selectedText);
-    unorderedDeepStrictEqual(result, [
-      { pair: "prop1={{ key: { key: { key: 'value' } } }}", prop: "prop1", value: "{{ key: { key: { key: 'value' } } }}", propAlias: "prop1" },
-      { pair: 'className="class-parent"', prop: "className", value: '"class-parent"', propAlias: "className" },
-      { pair: 'className={foo ? "class-1" : "class-2"}', prop: "className", value: '{foo ? "class-1" : "class-2"}', propAlias: "className2" },
-      {
-        pair: "prop1={{ key: { key: { key: 'value' } } }}",
-        prop: "prop1",
-        value: "{{ key: { key: { key: 'value' } } }}",
-        propAlias: "prop12",
-      },
-      {
-        pair: "prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
-        prop: "prop2",
-        value: "{[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
-        propAlias: "prop2",
-      },
-      {
-        pair: "prop3={() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
-        prop: "prop3",
-        value: "{() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
-        propAlias: "prop3",
-      },
-    ]);
+    unorderedDeepStrictEqual(
+      [
+        { pair: "prop1={{ key: { key: { key: 'value' } } }}", prop: "prop1", value: "{{ key: { key: { key: 'value' } } }}", propAlias: "prop1" },
+        { pair: 'className="class-parent"', prop: "className", value: '"class-parent"', propAlias: "className" },
+        { pair: 'className={foo ? "class-1" : "class-2"}', prop: "className", value: '{foo ? "class-1" : "class-2"}', propAlias: "className2" },
+        {
+          pair: "prop1={{ key: { key: { key: 'value' } } }}",
+          prop: "prop1",
+          value: "{{ key: { key: { key: 'value' } } }}",
+          propAlias: "prop12",
+        },
+        {
+          pair: "prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+          prop: "prop2",
+          value: "{[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+          propAlias: "prop2",
+        },
+        {
+          pair: "prop3={() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
+          prop: "prop3",
+          value: "{() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
+          propAlias: "prop3",
+        },
+      ],
+      result
+    );
   });
 
   test("should return an empty array if no props are found", () => {
@@ -106,7 +116,7 @@ suite("extractProps", () => {
       </Component>
     `;
     const result = extractProps(selectedText);
-    unorderedDeepStrictEqual(result, []);
+    unorderedDeepStrictEqual([], result);
   });
 });
 
@@ -184,7 +194,7 @@ suite("buildExtractedComponent", () => {
     `;
 
     const result = buildExtractedComponent(componentName, isTypescript, props, selectedText);
-    strictEqualStrippingLineBreaks(result, expected);
+    strictEqualStrippingLineBreaks(expected, result);
   });
 
   test("should build the extracted component with complex props and without interface, if using Javascript", () => {
@@ -215,7 +225,7 @@ suite("buildExtractedComponent", () => {
     `;
 
     const result = buildExtractedComponent(componentName, isTypescript, props, selectedText);
-    strictEqualStrippingLineBreaks(result, expected);
+    strictEqualStrippingLineBreaks(expected, result);
   });
 
   test("should build the extracted component without props and interface, if it does not have any props, regardless of using Typescript or not", () => {
@@ -234,10 +244,10 @@ suite("buildExtractedComponent", () => {
     `;
 
     const resultWithTypescript = buildExtractedComponent(componentName, true, props, selectedText);
-    strictEqualStrippingLineBreaks(resultWithTypescript, expected);
+    strictEqualStrippingLineBreaks(expected, resultWithTypescript);
 
     const resultWithoutTypescript = buildExtractedComponent(componentName, false, props, selectedText);
-    strictEqualStrippingLineBreaks(resultWithoutTypescript, expected);
+    strictEqualStrippingLineBreaks(expected, resultWithoutTypescript);
   });
 });
 
@@ -278,7 +288,7 @@ suite("buildExtractedComponentReference", () => {
       />`;
 
     const result = buildExtractedComponentReference(componentName, props);
-    strictEqualStrippingLineBreaks(result, expected);
+    strictEqualStrippingLineBreaks(expected, result);
   });
 
   test("should build the extracted component reference without props", () => {
@@ -288,6 +298,6 @@ suite("buildExtractedComponentReference", () => {
     const expected = `<Component />`;
 
     const result = buildExtractedComponentReference(componentName, props);
-    strictEqualStrippingLineBreaks(result, expected);
+    strictEqualStrippingLineBreaks(expected, result);
   });
 });
