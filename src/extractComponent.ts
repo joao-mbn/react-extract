ccd5508fba0118d23422d3d716aa4c3199be0e21
João Nascimento <joaombn97@gmail.com>
2024-02-11 17:37:48 -0300

    Adding user input for component name. Splitting extracted component builder and component reference builder on their own functions and adding tests to them.

--- src/extractComponent.ts
+++ src/extractComponent.ts
@@ -4,55 +4,29 @@ import XRegExp from "xregexp";
 import { isFileTypescript } from "./checks";
 import { ExtractedProps } from "./types";
 
-export function extractComponent(document: vscode.TextDocument, range: vscode.Range | vscode.Selection) {
-  const selectedText = document.getText(range).trim();
+export async function extractComponent(document: vscode.TextDocument, range: vscode.Range | vscode.Selection) {
+  const componentName = await vscode.window.showInputBox({ value: "Component", title: "Give the extracted component a name" });
 
-  const isTypescript = isFileTypescript(document);
-
-  // assuming the selection is JSX, extract props and props values, like <Component prop1="value1" prop2={value2} />
-  const props = extractProps(selectedText);
-
-  // Create a component with the name Component{5 random numbers/letters}
-  const componentName = `Component_${Math.random().toString(36).substring(2, 7).toUpperCase()}`;
-  // Add interface Component1Props if file is a TypeScript file
-  const interfaceName = `${componentName}Props`;
-
-  let newComponent = "";
-
-  if (isTypescript) {
-    newComponent += dedent`\n
-      interface ${interfaceName} {
-        ${props
-          .map((e) => `${e.propAlias}: unknown`)
-          .join(";\n")
-          .concat(";")}
-      }\n
-    `;
+  // If the user clears the input or cancels the input, it's implied that the user doesn't want to proceed.
+  if (!componentName) {
+    return;
   }
 
-  // replace selectedText substrings matching entries values with entries propAlias
-  const parsedText = props.reduce((accParsedText, currEntry) => accParsedText.replace(currEntry.value, `{${currEntry.propAlias}}`), selectedText);
+  const selectedText = document.getText(range).trim();
 
-  newComponent += dedent`\n
-    function ${componentName} ({
-      ${props.map((e) => e.propAlias).join(",\n")}
-    }${isTypescript ? `: ${interfaceName}` : ""}) {
-      return (
-        ${parsedText}
-      );
-    }`;
+  const props = extractProps(selectedText);
 
-  const newComponentReferenceOnParent = dedent`
-    <${componentName}
-      ${props.map((e) => `${e.pair}`).join("\n")}
-    />`;
+  const isTypescript = isFileTypescript(document);
+  const extractedComponent = buildExtractedComponent(componentName, isTypescript, props, selectedText);
+
+  const extractedComponentReference = buildExtractedComponentReference(componentName, props);
 
   // Add the content to the bottom of the same file
   const edit = new vscode.WorkspaceEdit();
-  edit.insert(document.uri, new vscode.Position(document.lineCount, 0), newComponent);
+  edit.insert(document.uri, new vscode.Position(document.lineCount, 0), extractedComponent);
 
   // Adds the new component instance, in place of the selected text
-  edit.replace(document.uri, range, newComponentReferenceOnParent);
+  edit.replace(document.uri, range, extractedComponentReference);
 
   vscode.workspace.applyEdit(edit);
 }
@@ -113,4 +87,46 @@ export function extractProps(selectedText: string): ExtractedProps[] {
         return { ...rest, propAlias: entry.prop };
       }) ?? []
   );
+}
+
+export function buildExtractedComponent(componentName: string, isTypescript: boolean, props: ExtractedProps[], selectedText: string) {
+  const interfaceName = `${componentName}Props`;
+
+  const interfaceDeclaration = dedent`\n
+    interface ${interfaceName} {
+      ${props
+        .map((e) => `${e.propAlias}: unknown`)
+        .join(";\n")
+        .concat(";")}
+      }\n
+  `;
+
+  // replace selectedText substrings matching entries values with entries propAlias
+  const parsedText = props.reduce((accParsedText, currEntry) => accParsedText.replace(currEntry.value, `{${currEntry.propAlias}}`), selectedText);
+
+  const shouldDisplayInterface = isTypescript && props.length > 0;
+
+  const functionDeclaration = dedent`
+    function ${componentName}(
+      ${props.length > 0 ? `{ ${props.map((e) => e.propAlias).join(",\n")} }` : ""}
+      ${shouldDisplayInterface ? `: ${interfaceName}` : ""}
+    ) {
+      return (
+        ${parsedText}
+      );
+    }
+  `;
+
+  return dedent`
+    ${shouldDisplayInterface ? interfaceDeclaration : ""}\n
+
+    ${functionDeclaration}
+  `;
+}
+
+export function buildExtractedComponentReference(componentName: string, props: ExtractedProps[]) {
+  return dedent`
+    <${componentName}
+      ${props.map((e) => `${e.pair}`).join("\n")}
+    />`;
 }
--- src/test/extractComponent.test.ts
+++ src/test/extractComponent.test.ts
@@ -1,10 +1,17 @@
 import * as assert from "assert";
-import { extractProps } from "../extractComponent";
+import { buildExtractedComponent, buildExtractedComponentReference, extractProps } from "../extractComponent";
 import { ExtractedProps } from "../types";
 
 function unorderedDeepStrictEqual(expected: ExtractedProps[], result: ExtractedProps[]) {
   return assert.deepStrictEqual(expected.map((e) => e.propAlias).sort(), result.map((e) => e.propAlias).sort());
 }
+
+function strictEqualStrippingLineBreaks(expected: string, result: string) {
+  const parser = (text: string) => text.replaceAll(/\s+(?=\W)|(?<=\W)\s+/g, "").trim();
+  return assert.strictEqual(parser(expected), parser(result));
+}
+
+// TODO: invert the order of arguments where results comes first
 
 suite("extractProps", () => {
   test("should handle props with different value types", () => {
@@ -100,5 +107,187 @@ suite("extractProps", () => {
     `;
     const result = extractProps(selectedText);
     unorderedDeepStrictEqual(result, []);
+  });
+});
+
+suite("buildExtractedComponent", () => {
+  const complexProps = [
+    { pair: "prop1={{ key: { key: { key: 'value' } } }}", prop: "prop1", value: "{{ key: { key: { key: 'value' } } }}", propAlias: "prop1" },
+    { pair: 'className="class-parent"', prop: "className", value: '"class-parent"', propAlias: "className" },
+    { pair: 'className={foo ? "class-1" : "class-2"}', prop: "className", value: '{foo ? "class-1" : "class-2"}', propAlias: "className2" },
+    {
+      pair: "prop1={{ key: { key: { key: 'value' } } }}",
+      prop: "prop1",
+      value: "{{ key: { key: { key: 'value' } } }}",
+      propAlias: "prop12",
+    },
+    {
+      pair: "prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+      prop: "prop2",
+      value: "{[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+      propAlias: "prop2",
+    },
+    {
+      pair: "prop3={() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return { item1: `${value1}`, item2: `${value2}` }; }}",
+      prop: "prop3",
+      value: "{() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return { item1: `${value1}`, item2: `${value2}` }; }}",
+      propAlias: "prop3",
+    },
+  ];
+
+  const complexComponent = `
+    <ParentComponent prop1={{ key: { key: { key: 'value' } } }} className="class-parent" >
+      <NestedComponent
+        className={foo ? "class-1" : "class-2"}
+        prop1={{ key: { key: { key: 'value' } } }}
+        prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}
+        prop3={() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return { item1: \`\${value1}\`, item2: \`\${value2}\` }; }}
+      />
+    </ParentComponent>
+  `;
+
+  test("should build the extracted component with complex props and interface, if using Typescript", () => {
+    const componentName = "Component";
+    const isTypescript = true;
+    const props = complexProps;
+    const selectedText = complexComponent;
+
+    const expected = `
+      interface ComponentProps {
+        prop1: unknown;
+        className: unknown;
+        className2: unknown;
+        prop12: unknown;
+        prop2: unknown;
+        prop3: unknown;
+      }
+
+      function Component({
+        prop1,
+        className,
+        className2,
+        prop12,
+        prop2,
+        prop3
+      }: ComponentProps) {
+        return (
+          <ParentComponent prop1={prop1} className={className} >
+            <NestedComponent
+              className={className2}
+              prop1={prop12}
+              prop2={prop2}
+              prop3={prop3}
+            />
+          </ParentComponent>
+        );
+      }
+    `;
+
+    const result = buildExtractedComponent(componentName, isTypescript, props, selectedText);
+    strictEqualStrippingLineBreaks(result, expected);
+  });
+
+  test("should build the extracted component with complex props and without interface, if using Javascript", () => {
+    const componentName = "Component";
+    const isTypescript = false;
+    const props = complexProps;
+    const selectedText = complexComponent;
+    const expected = `
+      function Component({
+        prop1,
+        className,
+        className2,
+        prop12,
+        prop2,
+        prop3
+      }) {
+        return (
+          <ParentComponent prop1={prop1} className={className} >
+            <NestedComponent
+              className={className2}
+              prop1={prop12}
+              prop2={prop2}
+              prop3={prop3}
+            />
+          </ParentComponent>
+        );
+      }
+    `;
+
+    const result = buildExtractedComponent(componentName, isTypescript, props, selectedText);
+    strictEqualStrippingLineBreaks(result, expected);
+  });
+
+  test("should build the extracted component without props and interface, if it does not have any props, regardless of using Typescript or not", () => {
+    const componentName = "Component";
+    const props: ExtractedProps[] = [];
+    const selectedText = "<AnotherComponent />";
+
+    const expected = `
+      function Component(
+
+      ) {
+        return (
+          <AnotherComponent />
+        );
+      }
+    `;
+
+    const resultWithTypescript = buildExtractedComponent(componentName, true, props, selectedText);
+    strictEqualStrippingLineBreaks(resultWithTypescript, expected);
+
+    const resultWithoutTypescript = buildExtractedComponent(componentName, false, props, selectedText);
+    strictEqualStrippingLineBreaks(resultWithoutTypescript, expected);
+  });
+});
+
+suite("buildExtractedComponentReference", () => {
+  test("should build the extracted component reference with props", () => {
+    const componentName = "Component";
+    const props: ExtractedProps[] = [
+      {
+        pair: "prop1={{ key: { key: { key: 'value' } } }}",
+        prop: "prop1",
+        value: "{{ key: { key: { key: 'value' } } }}",
+        propAlias: "prop12",
+      },
+      {
+        pair: "prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+        prop: "prop2",
+        value: "{[1, { key: { key: 'value' }, key2: 'value2' }, 3]}",
+        propAlias: "prop2",
+      },
+      {
+        pair: "prop3={() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
+        prop: "prop3",
+        value: "{() => { doStuff(); doAnotherStuff(); bar ? doBarStuff() : doFooStuff(); return {item1: `${value1}`, item2: `${value2}`}; }}",
+        propAlias: "prop3",
+      },
+    ];
+
+    const expected = `
+      <Component
+        prop1={{ key: { key: { key: 'value' } } }}
+        prop2={[1, { key: { key: 'value' }, key2: 'value2' }, 3]}
+        prop3={() => {
+          doStuff();
+          doAnotherStuff();
+          bar ? doBarStuff() : doFooStuff();
+          return {item1: \`\${value1}\`, item2: \`\${value2}\`};
+        }}
+      />`;
+
+    const result = buildExtractedComponentReference(componentName, props);
+    strictEqualStrippingLineBreaks(result, expected);
+  });
+
+  test("should build the extracted component reference without props", () => {
+    const componentName = "Component";
+    const props: ExtractedProps[] = [];
+
+    const expected = `<Component />`;
+
+    const result = buildExtractedComponentReference(componentName, props);
+    strictEqualStrippingLineBreaks(result, expected);
   });
 });
