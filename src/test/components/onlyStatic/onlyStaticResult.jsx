bfa0cf262f2eb93233b95dbdf1799171009cbf55
João Nascimento <joaombn97@gmail.com>
2024-02-16 09:55:23 -0300

    only static props test scenario.

---
+++ src/test/components/onlyStatic/onlyStaticResult.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CLASS_NAME, VALUE } from "./export";
+
+function Component() {
+  return (
+    <Extracted />
+  );
+}
+
+function Extracted() {
+  return (
+    <div className="my-class-2">
+      <div className="my-class">Test</div>
+      <span className={"string-literal"}></span>
+      <span className={`template literal`}></span>
+      <input min={0} max={VALUE} className={CLASS_NAME} />
+    </div>
+  );
+}
