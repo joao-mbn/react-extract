7c23e13ab9ada5a27f07236aa6726784765b9c7e
João Nascimento <joaombn97@gmail.com>
2024-02-16 10:05:23 -0300

    Added implicit true test case.

--- src/test/components/implicit/implicitResult.jsx
+++ src/test/components/implicit/implicitResult.jsx
@@ -4,10 +4,16 @@ function Component() {
   const isDisabled = Math.random() > 0.5;
 
   return (
+    <Extracted disabled={isDisabled} />
+  );
+}
+
+function Extracted({ disabled }) {
+  return (
     <form>
       <button disabled>Submit</button>
       <button disabled={false}>Do this</button>
-      <button disabled={isDisabled}>Do this</button>
+      <button disabled={disabled}>Do this</button>
       <input disabled placeholder="Type here" />
     </form>
   );
---
+++ src/test/components/implicit/implicitResult.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Component() {
+  const isDisabled = Math.random() > 0.5;
+
+  return (
+    <Extracted disabled={isDisabled} />
+  );
+}
+
+interface ExtractedProps {
+  disabled: boolean;
+}
+
+function Extracted({ disabled }: ExtractedProps) {
+  return (
+    <form>
+      <button disabled>Submit</button>
+      <button disabled={false}>Do this</button>
+      <button disabled={disabled}>Do this</button>
+      <input disabled placeholder="Type here" />
+    </form>
+  );
+}
